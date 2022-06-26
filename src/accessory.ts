1ec1b508c45bb88e6bc6d88f6bb72ca99abfcc0f
Arnold Kamminga <arnold.kamminga@hotmail.com>
2022-06-26 21:19:10 +0200

    thermostat with stubs

--- src/accessory.ts
+++ src/accessory.ts
@@ -2,13 +2,14 @@ import {
   AccessoryConfig,
   AccessoryPlugin,
   API,
+  Characteristic,
   CharacteristicEventTypes,
   CharacteristicGetCallback,
   CharacteristicSetCallback,
   CharacteristicValue,
   HAP,
   Logging,
-  Service
+  Service,
 } from "homebridge";
 
 /*
@@ -34,45 +35,86 @@ import {
  * like this for example and used to access all exported variables and classes from HAP-NodeJS.
  */
 let hap: HAP;
+var request = require('request');
 
 /*
  * Initializer function called when the plugin is loaded.
  */
 export = (api: API) => {
   hap = api.hap;
-  api.registerAccessory("ExampleSwitch", ExampleSwitch);
+  api.registerAccessory("DomoticzThermostat", ThermostatAccessory);
 };
 
-class ExampleSwitch implements AccessoryPlugin {
+class ThermostatAccessory implements AccessoryPlugin {
 
   private readonly log: Logging;
   private readonly name: string;
-  private switchOn = false;
+  private CurrentHeatingCoolingState=Characteristic.CurrentHeatingCoolingState.OFF;
+  private TargetHeatingCoolingState=Characteristic.TargetHeatingCoolingState.OFF;
+  private CurrentTemperature = 15;
+  private TargetTemperature = 25;
+  private TemperatureDisplayUnits = Characteristic.TemperatureDisplayUnits.CELSIUS;
 
-  private readonly switchService: Service;
+  private readonly ThermostatService: Service;
   private readonly informationService: Service;
+
 
   constructor(log: Logging, config: AccessoryConfig, api: API) {
     this.log = log;
     this.name = config.name;
 
-    this.switchService = new hap.Service.Switch(this.name);
-    this.switchService.getCharacteristic(hap.Characteristic.On)
+    this.ThermostatService = new hap.Service.Thermostat(this.name);
+
+    this.ThermostatService.getCharacteristic(hap.Characteristic.CurrentHeatingCoolingState)
       .on(CharacteristicEventTypes.GET, (callback: CharacteristicGetCallback) => {
-        log.info("Current state of the switch was returned: " + (this.switchOn? "ON": "OFF"));
-        callback(undefined, this.switchOn);
+        log.info("Getting current heating coooling state ");
+        callback(undefined, this.CurrentHeatingCoolingState);
+      });
+
+      this.ThermostatService.getCharacteristic(hap.Characteristic.TargetHeatingCoolingState)
+      .on(CharacteristicEventTypes.GET, (callback: CharacteristicGetCallback) => {
+        log.info("Getting target heating coooling state ");
+        callback(undefined, this.TargetHeatingCoolingState);
       })
       .on(CharacteristicEventTypes.SET, (value: CharacteristicValue, callback: CharacteristicSetCallback) => {
-        this.switchOn = value as boolean;
-        log.info("Switch state was set to: " + (this.switchOn? "ON": "OFF"));
+        this.TargetHeatingCoolingState = value as number;
+        log.info("Target Heating Cooling State was set to: " + this.TargetHeatingCoolingState);
         callback();
       });
 
-    this.informationService = new hap.Service.AccessoryInformation()
+      this.ThermostatService.getCharacteristic(hap.Characteristic.CurrentTemperature)
+      .on(CharacteristicEventTypes.GET, (callback: CharacteristicGetCallback) => {
+        log.info("Getting Current Temperature ");
+        callback(undefined, this.CurrentTemperature);
+      });
+
+      this.ThermostatService.getCharacteristic(hap.Characteristic.TargetTemperature)
+      .on(CharacteristicEventTypes.GET, (callback: CharacteristicGetCallback) => {
+        log.info("Getting Target Temperature ");
+        callback(undefined, this.TargetTemperature);
+      })
+      .on(CharacteristicEventTypes.SET, (value: CharacteristicValue, callback: CharacteristicSetCallback) => {
+        this.TargetTemperature = value as number;
+        log.info("Target Temperature set to: " + this.TargetTemperature);
+        callback();
+      });
+
+      this.ThermostatService.getCharacteristic(hap.Characteristic.TemperatureDisplayUnits)
+      .on(CharacteristicEventTypes.GET, (callback: CharacteristicGetCallback) => {
+        log.info("Getting Temperature Display Units ");
+        callback(undefined, this.TemperatureDisplayUnits);
+      })
+      .on(CharacteristicEventTypes.SET, (value: CharacteristicValue, callback: CharacteristicSetCallback) => {
+        this.TemperatureDisplayUnits = value as number;
+        log.info("Temperature Display Units was set to: " + this.TemperatureDisplayUnits);
+        callback();
+      });
+
+      this.informationService = new hap.Service.AccessoryInformation()
       .setCharacteristic(hap.Characteristic.Manufacturer, "Custom Manufacturer")
       .setCharacteristic(hap.Characteristic.Model, "Custom Model");
 
-    log.info("Switch finished initializing!");
+    log.info("Thermostat finished initializing!");
   }
 
   /*
@@ -90,7 +132,7 @@ class ExampleSwitch implements AccessoryPlugin {
   getServices(): Service[] {
     return [
       this.informationService,
-      this.switchService,
+      this.ThermostatService,
     ];
   }
 
